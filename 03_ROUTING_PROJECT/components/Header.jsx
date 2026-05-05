459d74bbe6a422d445d7679de62992f1d921e936
mahammedaadil patel <adilchoice30@gmail.com>
2026-05-05 13:56:23 +0530

    created small static project and learned how Link element work in nextJS,also got idea about how we can copy tailwind components from websites

---
+++ 03_ROUTING_PROJECT/components/Header.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+const Header = () => {
+  return (
+    <div>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+            <Link href={"/"} className="mr-5 hover:text-gray-900">
+              Home
+            </Link>
+            <Link href={"/about"} className="mr-5 hover:text-gray-900">
+              About
+            </Link>
+            <Link href={"/contact"} className="mr-5 hover:text-gray-900">
+              Contact
+            </Link>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
