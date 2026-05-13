e4c9175459234a4bad9d4a4a3c2ccd83246f2d46
mahammedaadil patel <adilchoice30@gmail.com>
2026-05-13 18:21:16 +0530

    remove console log for database connection

--- project-notes-app/lib/db.js
+++ project-notes-app/lib/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export async function connectDb() {
   try {
     await mongoose.connect("mongodb://localhost:27017/project-notes-app");
-    console.log("DB is connected");
   } catch (error) {
     throw new error(error);
   }
