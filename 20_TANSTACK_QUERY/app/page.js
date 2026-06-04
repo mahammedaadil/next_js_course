25de4ed4f4ec8d86c30f21852ad4bd628821e05b
mahammedaadil patel <adilchoice30@gmail.com>
2026-06-04 19:30:04 +0530

    understood tanstack query how it works how can we create providers and wrap to our app, also understood that how we can fetch using useQuery ,QueryClientProvider,QueryClient

---
+++ 20_TANSTACK_QUERY/app/page.js
@@ -0,0 +1,50 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+export default function Home() {
+  //normal way too much boilerplate code
+
+  // const [data, setData] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  // const fetchApi = async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // eslint-disable-next-line react-hooks/set-state-in-effect
+  //   fetchApi();
+  // }, []);
+
+
+  //less boiler plate with tanstack
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["data"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+        res.json(),
+      ),
+  });
+
+  if (isLoading) {
+    return <div>Loading.....</div>;
+  }
+
+  if (error) {
+    return <div>Error.....</div>;
+  }
+
+  return (
+    <div>
+      <h1>{JSON.stringify(data)}</h1>
+    </div>
+  );
+}
